85b5a310cdf75f2a38987e7508679133f2c5f3d9
Shota Kuroki <kurokis@users.noreply.github.com>
2021-04-05 21:42:00 +0900

    Updated dependencies for asammdf

--- setup_files/setup.bat
+++ setup_files/setup.bat
@@ -3,15 +3,15 @@ cd /d %~dp0
 cd /d ..
 
 rem pipをアップグレード
-py -m pip install --upgrade pip
+pip install --upgrade pip
 
 rem pipenvをインストールする
-py -m pip install pipenv 
+pip install pipenv 
 
 rem 環境変数を変更し、カレントディレクトリ以下に仮想環境をインストールさせる
 set PIPENV_VENV_IN_PROJECT=1
 
-rem 仮想環境をデプロイ(--deployオプションによりPipfile.lockの依存関係が古い場合はstatus_code 1を返して終了)
-py -m pipenv install --deploy
+rem 仮想環境をデプロイ
+pipenv install
 
 pause
--- setup_files/setup_dev.bat
+++ setup_files/setup_dev.bat
@@ -3,15 +3,15 @@ cd /d %~dp0
 cd /d ..
 
 rem pipをアップグレード
-C:\Windows\py.exe -m pip install --upgrade pip
+pip install --upgrade pip
 
 rem pipenvをインストールする
-C:\Windows\py.exe -m pip install pipenv 
+pip install pipenv 
 
 rem 環境変数を変更し、カレントディレクトリ以下に仮想環境をインストールさせる
 set PIPENV_VENV_IN_PROJECT=1
 
 rem 仮想環境をデプロイ(--devオプションで開発用パッケージもインストールする)
-C:\Windows\py.exe -m pipenv install --dev
+pipenv install --dev
 
 pause
